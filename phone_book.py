031082de013eb3b4493aa114b0924388c3d49a68
Yisaterese <teresejosephyisa@gmail.com>
2024-02-07 00:19:59 +0100

    added code

--- phone_book.py
+++ phone_book.py
@@ -1,32 +1,44 @@
 import sys
 
 
-class phone_book:
-    def __init__(self,contacts_list = []):
+class PhoneBook:
+    def __init__(self, contacts_list=[]):
         self.contacts_list = contacts_list
 
-    def add_contacts(self,name,phone_number):
 
-        for index in range (len(name)):
-            if len(name) != 11:
-                return "Enter a valid number with eleven digits"
-        contact = {"name":name,"phone_umber":phone_number}
-        self.contacts_list.append(contact)
+    def add_contacts(self, name, phone_number):
+        if len(phone_number) == 11 and phone_number.startswith("0"):
+            contact = {"name": name, "phone_number": phone_number}
+            self.contacts_list.append(contact)
+        return len(self.contacts_list)
 
-    def search_contact(self,name):
-        for contact in self.contacts_list:
-           if contact["name"] == name:
-               return contact.get(name)
-
-
-    def delete_contact(self,delete_contact):
+    def delete_contact(self, delete_contact):
         for contact in self.contacts_list:
             if contact["name"] == delete_contact:
                 self.contacts_list.remove(contact)
 
-    def exit(self,sentinel):
 
-        def contact_menu(self,option):
+'''  def search_contact(self, name):
+        for contact in self.contacts_list:
+            if contact["name"] == name:
+                return contact.get(name)
+            else:
+                return "contact not found"
+
+    def display_contact(self, contacts_list):
+        contact_lst = []
+        for numbers in range(len(contacts_list)):
+            if numbers in contacts_list:
+                contact_lst.append(numbers)
+                return contact_lst
+            else:
+                return "contact not found"
+
+   
+
+    def exit(self, sentinel):
+
+        def contact_menu(self, option):
             sys.exit()
 
             while True:
@@ -50,8 +62,4 @@ class phone_book:
                     exit()
                     print("Goodbye thanks for banking with us")
             else:
-                print("select a valid option")
-
-
-
-
+                print("select a valid option")'''
--- test_phone_book.py
+++ test_phone_book.py
@@ -1,11 +1,16 @@
 
-from phone_book import *
+from phone_book import PhoneBook
 
-def test_save_contacts():
-    book = phone_book()
-    book.add_contacts("Ada","57890876543")
-    added_contact = book.search_contact("Ada")
-    assert  added_contact == "Ada"
+def test_if_phone_book_can_add_contacts():
+    book = PhoneBook()
+    assert book.add_contacts("Ada","07890876543") == 1
+    assert book.add_contacts("Ada","07890876543") == 2
+    assert book.add_contacts("john", "07890876543") == 3
 
+def test_display_contact_list():
+    book = PhoneBook()
+    assert book.add_contacts("Ada", "07890876543") == 1
+    assert book.add_contacts("Ada", "07890876543") == 2
+    assert book.add_contacts("john", "07890876543") == 3
 
-
+    assert book.delete_contact("Ada") == 2
