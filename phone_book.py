4c3bcf6eeac9ea9328ead2559d9ccaed67686829
Yisaterese <teresejosephyisa@gmail.com>
2024-02-06 06:29:07 +0100

    added code

--- phone_book.py
+++ phone_book.py
@@ -1,23 +1,55 @@
+import sys
+
+
 class phone_book:
     def __init__(self,contacts_list = []):
         self.contacts_list = contacts_list
 
-    def save_contacts(self,name,phone_number):
-        for index in range(len(name)):
-            if index != 11:
+    def add_contacts(self,name,phone_number):
+        for index in range (len(name)):
+            if len(phone_book) != 11:
                 return "Enter a valid number with eleven digits"
         contact = {"name":name,"phone_umber":phone_number}
         self.contacts_list.append(contact)
 
-    def serch_contact(self,name):
+    def search_contact(self,name):
         for contact in self.contacts_list:
            if contact["name"] == name:
                return contact
 
+
     def delete_contact(self,delete_contact):
         for contact in self.contacts_list:
             if contact["name"] == delete_contact:
-                self.contacts_list.pop(delete_contact)
+                self.contacts_list.remove(contact)
+
+    def exit(self,sentinel):
+
+        def contact_menu(self,option):
+            sys.exit()
+
+            while True:
+                print("""1. Add contact
+                         2. Delete contact
+                         3. Search contact
+                         4. Exit
+                           """)
+                option = input(int("select option between 1 and 4: "))
+                if option == 1:
+                    self.add_contact()
+                    self.contact_menu()
+                elif option == 2:
+                    self.dellete_contact()
+                    self.contact_menu()
+                elif option == 3:
+                    self.serch_contact()
+                    self.contact_menu()
+                elif option == 4:
+                    break
+                    exit()
+                    print("Goodbye thanks for banking with us")
+            else:
+                print("select a valid option")
 
 
 
--- test_phone_book.py
+++ test_phone_book.py
@@ -1,12 +1,11 @@
 
-import pytest
 from phone_book import *
 
-#@pytest.fixture
-
 def test_save_contacts():
-    phone_book.save_contacts(524238162353,"ada",)
-    assert "ada",524238162353 in  phone_book.save_contacts( phone_book)
+    book = phone_book()
+    book.add_contacts("Ada","57890876543")
+    added_contact = book.search_contact({"Ada":"57890876543"})
+    assert "ada" ==  added_contact
 
 
 
