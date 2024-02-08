4d7a16086ba9ec8d2757a946b1a304d4f0ee334a
Yisaterese <teresejosephyisa@gmail.com>
2024-02-08 07:03:30 +0100

    added code

--- phone_book/phone_book.py
+++ phone_book/phone_book.py
@@ -43,32 +43,3 @@ class PhoneBook:
     #         else:
     #             return "contact not found"
     #
-    #
-    #
-    # def exit(self, sentinel):
-    #
-    #     def contact_menu(self, option):
-    #         sys.exit()
-    #
-    #         while True:
-    #             print("""1. Add contact
-    #                      2. Delete contact
-    #                      3. Search contact
-    #                      4. Exit
-    #                        """)
-    #             option = input(int("select option between 1 and 4: "))
-    #             if option == 1:
-    #                 self.add_contact()
-    #                 self.contact_menu()
-    #             elif option == 2:
-    #                 self.dellete_contact()
-    #                 self.contact_menu()
-    #             elif option == 3:
-    #                 self.serch_contact()
-    #                 self.contact_menu()
-    #             elif option == 4:
-    #                 break
-    #                 exit()
-    #                 print("Goodbye thanks for banking with us")
-    #         else:
-    #             print("select a valid option")
