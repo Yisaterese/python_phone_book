b9e5e603821c756d5e6926a75e3f9767acae768a
Yisaterese <teresejosephyisa@gmail.com>
2024-02-08 05:33:06 +0100

    added code

--- phone_book/phone_book.py
+++ phone_book/phone_book.py
@@ -4,77 +4,74 @@ class PhoneBook:
 
     def __init__(self, contacts_list=[]):
         self.contacts_list = contacts_list
+        #self.list_of_contacts = len(self.contacts_list)
 
     def add_contacts(self, name, phone_number):
         if len(phone_number) == 11 and phone_number.startswith("0"):
-            contact = {"name": name, "phone_number": phone_number}
+            contact = {"name": name, "1phone_number": phone_number}
             self.contacts_list.append(contact)
 
-    def list_of_contacts(self):
-        if self.contacts_list:
-            return len(self.contacts_list)
+    def length_of_contact_list(self):
+        if not self.contacts_list:
+            return 0
         else:
-            return "contact list empty"
+            return len(self.contacts_list)
 
     def delete_contact(self, delete_name):
-        for contact in self.contacts_list:
-            if contact["name"] == delete_name:
-                self.contacts_list.remove(contact)
+        for the_contacts in self.contacts_list:
+            if the_contacts["name"] == delete_name:
+                self.contacts_list.remove(the_contacts)
                 break
 
 
     def search_contact(self, name):
-        for contact in self.contacts_list:
-            if contact["name"] == name:
-                return contact.get(name)
-            else:
-                return "contact not found"
+        for my_contacts in self.contacts_list:
+            if my_contacts["name"] == name:
+                return True
+
+        return False
 
 
     def clear_contact_list(self):
         if self.contacts_list:
-            self.contacts_list.clear(self)
-        else:
-            return "contact list already empty"
+            self.contacts_list.clear()
 
-
-''' 
-
-    def display_contact(self, contacts_list):
-        contact_lst = []
-        for numbers in range(len(contacts_list)):
-            if numbers in contacts_list:
-                contact_lst.append(numbers)
-                return contact_lst
-            else:
-                return "contact not found"
-
-   
-
-    def exit(self, sentinel):
-
-        def contact_menu(self, option):
-            sys.exit()
-
-            while True:
-                print("""1. Add contact
-                         2. Delete contact
-                         3. Search contact
-                         4. Exit
-                           """)
-                option = input(int("select option between 1 and 4: "))
-                if option == 1:
-                    self.add_contact()
-                    self.contact_menu()
-                elif option == 2:
-                    self.dellete_contact()
-                    self.contact_menu()
-                elif option == 3:
-                    self.serch_contact()
-                    self.contact_menu()
-                elif option == 4:
-                    break
-                    exit()
-                    print("Goodbye thanks for banking with us")
-            else:
-                print("select a valid option")'''
+    #
+    # def display_contact(self, contacts_list):
+    #     contact_lst = []
+    #     for numbers in range(len(contacts_list)):
+    #         if numbers in contacts_list:
+    #             contact_lst.append(numbers)
+    #             return contact_lst
+    #         else:
+    #             return "contact not found"
+    #
+    #
+    #
+    # def exit(self, sentinel):
+    #
+    #     def contact_menu(self, option):
+    #         sys.exit()
+    #
+    #         while True:
+    #             print("""1. Add contact
+    #                      2. Delete contact
+    #                      3. Search contact
+    #                      4. Exit
+    #                        """)
+    #             option = input(int("select option between 1 and 4: "))
+    #             if option == 1:
+    #                 self.add_contact()
+    #                 self.contact_menu()
+    #             elif option == 2:
+    #                 self.dellete_contact()
+    #                 self.contact_menu()
+    #             elif option == 3:
+    #                 self.serch_contact()
+    #                 self.contact_menu()
+    #             elif option == 4:
+    #                 break
+    #                 exit()
+    #                 print("Goodbye thanks for banking with us")
+    #         else:
+    #             print("select a valid option")
--- phone_book/test_phone_book.py
+++ phone_book/test_phone_book.py
@@ -6,26 +6,51 @@ from phone_book import PhoneBook
 
 def test_if_phone_book_can_add_contacts():
     phone_book = PhoneBook()
+    assert phone_book.length_of_contact_list() == 0
     phone_book.add_contacts("Ada", "07890876543")
-    assert phone_book.list_of_contacts() == 1
+    assert phone_book.length_of_contact_list() == 1
+
+
+def test_phone_book_is_empty_before_adding_contact():
+    phone_book2 = PhoneBook()
+    assert phone_book2.length_of_contact_list() == 0
 
 
 def test_contact_can_be_deleted():
 
-    phone_book = PhoneBook()
-    phone_book. add_contacts("Ada", "07890876543")
-    phone_book.add_contacts("john", "07890876543")
-    phone_book.delete_contact("Ada")
-    assert phone_book.list_of_contacts() == 1
+    phone_book1 = PhoneBook()
+    assert phone_book1.length_of_contact_list() == 0
+    phone_book1. add_contacts("Ada", "07890876543")
+    phone_book1.add_contacts("john", "07890876543")
+    assert phone_book1.length_of_contact_list() == 2
+
+    phone_book1.delete_contact("Ada")
+    assert phone_book1.length_of_contact_list() == 1
+
+    phone_book1.clear_contact_list()
+    assert phone_book1.length_of_contact_list() == 0
+
 
 def test_can_search_for_contacts():
     phone_book = PhoneBook()
     phone_book.add_contacts("Ada", "07890876543")
     phone_book.add_contacts("john", "07890876543")
-    phone_book.delete_contact("Ada")
-    assert phone_book.list_of_contacts() == 1
-    assert phone_book.search_contact("john") == "contact name exists in phone book"
+    assert phone_book.search_contact("john") == True
+    assert phone_book.length_of_contact_list() == 2
 
+    phone_book.clear_contact_list()
+    assert phone_book.length_of_contact_list() == 0
+
+
+def test_can_clear_all_contact_list():
+    phone_book = PhoneBook()
+
+    assert phone_book.length_of_contact_list() == 0
+    phone_book.add_contacts("Ada", "07890876543")
+    phone_book.add_contacts("john", "07890876543")
+
+    phone_book.clear_contact_list()
+    assert phone_book.length_of_contact_list() == 0
 
 
 
